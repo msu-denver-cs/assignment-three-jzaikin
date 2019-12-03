678d1c811c8247e26b8e61c22286cc0fdf35177c
jzaikin <jzaikin@msudenver.edu>
2019-12-03 13:09:27 -0700

    Add Devise Add navigator

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,5 +6,4 @@ class Car < ApplicationRecord
   has_and_belongs_to_many :parts
   validates :parts, presence: true
 
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 Rails.application.routes.draw do
-  resources :makes do
-    collection do
-      get 'search'
-    end
-  end
-  resources :parts do
-    collection do
-      get 'search'
-    end
-  end
-  resources :cars do
-    collection do
-      get 'search'
-    end
-  end
-  resources :car_makes
-  resources :car_parts
-
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Devise asked for this
+  root to: "home#cars"
 end
